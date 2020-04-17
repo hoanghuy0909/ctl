82b1338531d34e4b80015c2c6a1cd9dd58694792
Hoang Quang huy <hoangquanghuya6@gmail.com>
2020-04-17 15:10:33 +0700

    Initial commit

---
+++ baitapnhom/frmbaocaotheothangquynam.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace baitapnhom
+{
+    partial class frmbaocaotheothangquynam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtdtt = new System.Windows.Forms.TextBox();
+            this.txtnam = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "hiển thị theo tháng";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtdtt
+            // 
+            this.txtdtt.Location = new System.Drawing.Point(65, 48);
+            this.txtdtt.Name = "txtdtt";
+            this.txtdtt.Size = new System.Drawing.Size(100, 20);
+            this.txtdtt.TabIndex = 3;
+            // 
+            // txtnam
+            // 
+            this.txtnam.Location = new System.Drawing.Point(65, 93);
+            this.txtnam.Name = "txtnam";
+            this.txtnam.Size = new System.Drawing.Size(100, 20);
+            this.txtnam.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tháng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Năm";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(195, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(176, 167);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(296, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Xuất file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(44, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "hiển thị theo năm";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(75, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Báo cáo tổng tiền thu ";
+            // 
+            // frmbaocaotheothangquynam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 307);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtnam);
+            this.Controls.Add(this.txtdtt);
+            this.Controls.Add(this.button1);
+            this.Name = "frmbaocaotheothangquynam";
+            this.Text = "frmbaocaotheothangquynam";
+            this.Load += new System.EventHandler(this.frmbaocaotheothangquynam_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtdtt;
+        private System.Windows.Forms.TextBox txtnam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ baitapnhom/frmbaocaotheothangquynam.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace baitapnhom
+{
+    public partial class frmbaocaotheothangquynam : Form
+    {
+        public frmbaocaotheothangquynam()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hienthi();
+        }
+
+        private void frmbaocaotheothangquynam_Load(object sender, EventArgs e)
+        {
+            DAO.connect();
+           
+        }
+        private void hienthi()
+        {
+            string sql = "select sum(DATEDIFF(day,c.ngaythue,f.ngaytra)*(a.dongiathue)+i.tienphat) as [doanh thu la:] from sachtruyen a join chitietthuesach b on a.masach = b.masach join thuesach c on c.mathue = b.mathue join chitiettrasach d on d.masach = a.masach join trasach f on f.matra = d.matra join vipham i on i.mavipham = d.mavipham where datepart(month,f.ngaytra)='" + txtdtt.Text + "' and datepart(year, f.ngaytra) = '" + txtnam.Text + "'";
+            SqlDataAdapter adp = new SqlDataAdapter(sql, DAO.con);
+            DataTable tblchitiettrasach = new DataTable();
+            adp.Fill(tblchitiettrasach);
+            dataGridView1.DataSource = tblchitiettrasach;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sql = "select sum(DATEDIFF(day,c.ngaythue,f.ngaytra)*(a.dongiathue)+i.tienphat) as [doanh thu la:] from sachtruyen a join chitietthuesach b on a.masach = b.masach join thuesach c on c.mathue = b.mathue join chitiettrasach d on d.masach = a.masach join trasach f on f.matra = d.matra join vipham i on i.mavipham = d.mavipham where datepart(year, f.ngaytra) = '" + txtnam.Text + "'";
+            SqlDataAdapter adp = new SqlDataAdapter(sql, DAO.con);
+            DataTable tblchitiettrasach = new DataTable();
+            adp.Fill(tblchitiettrasach);
+            dataGridView1.DataSource = tblchitiettrasach;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            exexcel ex = new exexcel();
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            ex.Export(dt, "Danh sach", "Báo cáo tổng tiền thu");
+        }
+    }
+}
